bc7ed608a5c2c0328f49de3c67d01bbedb8f239f
Eduardo San Martin Morote <posva13@gmail.com>
2013-12-22 22:23:04 +0100

    new test

---
+++ tests/tcatimg.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+IMG=data/yoshi.png
+./bin/sh2d $IMG > tmp
+W=`convert "$IMG" -print "%w\n" /dev/null`
+H=`convert "$IMG" -print "%h\n" /dev/null`
+echo "$IMG: ${W}x${H}"
+
+# Dimensions test
+[[ `wc -l < tmp` -eq "$H" ]] || exit 1
+[[ `sed 's/\[[0-9][0-9;m]* */-/g' < tmp | head -n 1 | wc -c` -eq `expr $W + 2` ]] || exit 1
