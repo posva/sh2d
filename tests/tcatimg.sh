4bcc08dd3652e1a0ec6138d43555d8d55aa483dd
Eduardo San Martin Morote <posva13@gmail.com>
2013-12-22 22:31:41 +0100

    for some reason travis didn't like my test

--- tests/tcatimg.sh
+++ tests/tcatimg.sh
@@ -7,12 +7,16 @@ H=`convert "$IMG" -print "%h\n" /dev/null`
 echo "$IMG: ${W}x${H}"
 
 function gtfo() {
-  rm tmp
-  exit 1
+  if [[ ! "$1" ]]; then
+    echo "Error"
+  fi
+  if [[ -f tmp ]]; then
+    rm tmp
+  fi
 }
 
 # Dimensions test
 [[ `wc -l < tmp` -eq "$H" ]] || ( gtfo && exit 1)
 [[ `sed 's/\[[0-9][0-9;m]* */-/g' < tmp | head -n 1 | wc -c` -eq `expr $W + 2` ]] || ( gtfo && exit 1)
 
-rm tmp
+gtfo 1
