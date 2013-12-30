0478a6a42fb3a48699eee40289509a9cdd19b230
Eduardo San Martin Morote <posva13@gmail.com>
2013-12-30 12:37:41 +0100

    Using Cmake

--- tests/tcatimg.sh
+++ tests/tcatimg.sh
@@ -1,11 +1,5 @@
 #! /bin/bash
 
-IMG=data/yoshi.png
-./bin/sh2d $IMG > tmp
-W=`convert "$IMG" -print "%w\n" /dev/null`
-H=`convert "$IMG" -print "%h\n" /dev/null`
-echo "$IMG: ${W}x${H}"
-
 function gtfo() {
   if [[ ! "$1" ]]; then
     echo "Error"
@@ -15,8 +9,34 @@ function gtfo() {
   fi
 }
 
+function pre_test() {
+  echo -ne "\e[33m$1\e[0m"
+}
+
+function ok_test() {
+echo -e "\e[32mOK\e[0m"
+}
+
+IMG=data/yoshi.png
+EXC="./bin/catimg"
+if [[ ! -x $EXC ]]; then
+    gtfo && exit 1
+fi
+${EXC} $IMG > tmp
+W=`convert "$IMG" -print "%w\n" /dev/null`
+H=`convert "$IMG" -print "%h\n" /dev/null`
+echo "$IMG: ${W}x${H}"
+
 # Dimensions test
-[[ `wc -l < tmp` -eq "$H" ]] || ( gtfo && exit 1)
-[[ `sed 's/\[[0-9][0-9;m]* */-/g' < tmp | head -n 1 | wc -c` -eq `expr $W + 2` ]] || ( gtfo && exit 1)
+pre_test "Lines..."
+if [[ ! `wc -l < tmp` -eq "$H" ]]; then
+  gtfo && exit 1
+fi
+ok_test
+pre_test "Columns..."
+if [[ ! `sed 's/\[[0-9][0-9;m]* */-/g' < tmp | head -n 1 | wc -c` -eq `expr $W + 2` ]]; then
+  gtfo && exit 1
+fi
+ok_test
 
 gtfo 1
