76eb61741f6bd32d68da3bf1e643492be334172e
Eduardo San Martin Morote <posva13@gmail.com>
2013-12-30 17:32:48 +0100

    render still empty

---
+++ src/sh_render.h
@@ -0,0 +1,20 @@
+#ifndef __SH_RENDER_H__
+#define __SH_RENDER_H__
+/*******************************************************************************
+ *  sh_render                                                                  *
+ *  Mon Dec 30 CET 2013                                                        *
+ *  Copyright Eduardo San Martin Morote                                        *
+ *  eduardo.san-martin-morote@ensimag.fr                                       *
+ *  http://posva.net                                                           *
+ ******************************************************************************/
+
+#include "sh_image.h"
+
+/**
+ * @defgroup sh_renderer sh_renderer
+ * @{
+ */
+
+
+///@}
+#endif
